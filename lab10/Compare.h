31469461fc967be2645e912d50dc4b22c648dcc9
VarzarMiruna <125951638+VarzarMiruna@users.noreply.github.com>
2024-05-16 11:36:50 +0300

    Add files via upload

---
+++ lab10/Compare.h
@@ -0,0 +1,11 @@
+#pragma once
+class Compare
+{
+public:
+	virtual int CompareElements(void* e1, void* e2) = 0;
+};
+
+class CompareGreater : public Compare
+{
+	virtual int CompareElements(void* e1, void* e2) override;
+};
